74409c6f214043fdf37c63af94bbd773cdc4738d
kzwchen <katherine.zw.chen@gmail.com>
2020-12-22 12:31:13 -0500

    tried to adjust teleop
    Used: https://www.learnroadrunner.com/advanced.html#using-road-runner-in-teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
@@ -4,36 +4,44 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Hardware.BFRMecanumDrive;
+import org.firstinspires.ftc.teamcode.Hardware.RobotHardware;
 import org.firstinspires.ftc.teamcode.Utility.FrogOpMode;
 @TeleOp
 public class Teleop extends FrogOpMode {
-    private BFRMecanumDrive drive = null;
     @Override
     public void initialize() {
-        drive = new BFRMecanumDrive(hardwareMap);
-
+        BFRMecanumDrive drive = RobotHardware.getInstance().drive;
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-
     @Override
     public void repeat()  {
         telemetry.addData("Pigs", "%f, %f", gamepad1.left_stick_x, gamepad1.left_stick_y);
         telemetry.update();
+        BFRMecanumDrive drive = RobotHardware.getInstance().drive;
 
-        if(gamepad1.left_stick_y != 0 || gamepad1.left_stick_x != 0) {
-            Vector2d direction = new Vector2d(-0.1 * gamepad1.left_stick_x, -0.1 * gamepad1.left_stick_y);
-            Trajectory trajectory = drive.trajectoryBuilder(new Pose2d())
-                    .strafeTo(direction)
-                    .build();
-            drive.followTrajectory(trajectory);
+        // Read pose
+        Pose2d poseEstimate = drive.getPoseEstimate();
 
-            try {
-                wait(2000);
-            }
-            catch(Exception e)
-            {}
-        }
+        // Create a vector from the gamepad x/y inputs
+        // Then, rotate that vector by the inverse of that heading
+        Vector2d input = new Vector2d(
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x
+        );
+
+        // Pass in the rotated input + right stick value for rotation
+        // Rotation is not part of the rotated input thus must be passed in separately
+        drive.setWeightedDrivePower(
+                new Pose2d(
+                        input.getX(),
+                        input.getY(),
+                        -gamepad1.right_stick_x
+                )
+        );
+
     }
 }
 
