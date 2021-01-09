afb78a99b8493dde9fcd9c28c0c7b84b27c0e0f5
kzwchen <katherine.zw.chen@gmail.com>
2021-01-08 21:59:43 -0500

    found servo start positions
    0.05 for incline servo, 0.55 for swiper servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTests/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTests/ServoTest.java
@@ -14,12 +14,13 @@ public class ServoTest extends FrogOpMode {
     @Override
     public void repeat() {
         RobotHardware robot = RobotHardware.getInstance();
-        robot.basket.inclineServo.setPosition(0);
+        robot.basket.inclineServo.setPosition(0.05);
 
-        robot.basket.inclineServo.setPosition(0);
+        robot.basket.swiperServo.setPosition(0.55);
 //
 //        telemetry.addData("incline servo position", inclineServoPos);
 //        telemetry.addData("swiper servo position", swiperServoPos);
-//        telemetry.update();
+//        telemetry.update()
+        //0.05 and 0.55 are the starting positions for the incline and swiper servos
     }
 }
